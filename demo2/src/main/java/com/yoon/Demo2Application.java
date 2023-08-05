a000566cb8d1859e453dc62d8e287b1a511d662b
6161990 <6161990src@gmail.com>
2023-08-05 21:37:43 +0900

    setting dependencies

--- demo2/src/main/java/com/yoon/Demo2Application.java
+++ demo2/src/main/java/com/yoon/Demo2Application.java
@@ -1,7 +1,13 @@
 package com.yoon;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Demo2Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Demo2Application.class, args);
     }
+
 }
